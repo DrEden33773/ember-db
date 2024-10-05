0535019a208f02d662e57f0706e4401491ac5ec1
Eden Wang <edwardwang33773@gmail.com>
2024-10-05 18:42:09 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![warn(clippy::all)]
+#![allow(clippy::new_without_default)]
+#![allow(clippy::too_long_first_doc_paragraph)]
+
+pub mod client;
+pub mod encoding;
+pub mod error;
+pub mod raft;
+pub mod server;
+pub mod sql;
+pub mod storage;
